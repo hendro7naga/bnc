f6e866b5d3c7a7630c20db49aec5f55734404034
xxcrashblackxx <crashblack160@gmail.com>
2015-12-16 02:44:27 +0700

    Create detailsub.php

---
+++ include/detailsub.php
@@ -0,0 +1,85 @@
+<?php
+require_once("./convikx/apps.php");
+?>
+	<!--=== Breadcrumbs ===-->
+	<div class="breadcrumbs">
+	    <div class="container">
+	        <h1 class="pull-left"><?php echo $kontrol->breadcrumbsNama($_GET['q']); ?></h1>
+	        <ul class="pull-right breadcrumb">
+	            <li><a href="index.html">Beranda</a></li>
+	            <li class="active"><?php echo $kontrol->breadcrumbsNama($_GET['q']); ?></li>
+	        </ul>
+	    </div><!--/container-->
+	</div><!--/breadcrumbs-->
+	<!--=== End Breadcrumbs ===-->
+
+     <!--=== Content Part ===-->
+    <div class="container content">		
+    	<div class="row blog-page blog-item">
+            <!-- Left Sidebar -->
+        	<div class="col-md-9 md-margin-bottom-60">
+                <!--Blog Post-->        
+                <?php echo $kontrol->detailSubMenu($_GET['id']); ?>
+                <!--End Blog Post-->        
+
+    			<hr>
+            </div>
+            <!-- End Left Sidebar -->
+
+            <!-- Right Sidebar -->
+            <div class="col-md-3 magazine-page">
+
+                <!-- Posts -->
+                <div class="posts margin-bottom-40">
+                    <div class="headline headline-md"><h2>Topik Terkait</h2></div>
+                    <dl class="dl-horizontal">
+                        <dt><a href="#"><img src="assets/img/sliders/elastislide/6.jpg" alt="" /></a></dt>
+                        <dd>
+                            <p><a href="#">Responsive Bootstrap 3 Template placerat idelo alac eratamet.</a></p> 
+                        </dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt><a href="#"><img src="assets/img/sliders/elastislide/10.jpg" alt="" /></a></dt>
+                        <dd>
+                            <p><a href="#">100+ Amazing Features Layer Slider, Layer Slider, Icons, 60+ Pages etc.</a></p> 
+                        </dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt><a href="#"><img src="assets/img/sliders/elastislide/11.jpg" alt="" /></a></dt>
+                        <dd>
+                            <p><a href="#">Developer Friendly Code imperdiet condime ntumi mperdiet condim.</a></p> 
+                        </dd>
+                    </dl>
+                </div><!--/posts-->
+                <!-- End Posts -->
+
+
+                <!-- Post sebelumnya -->
+                <div class="posts margin-bottom-40">
+                    <div class="headline headline-md"><h2>Topik Lainnya</h2></div>
+                    <dl class="dl-horizontal">
+                        <dt><a href="#"><img src="assets/img/sliders/elastislide/6.jpg" alt="" /></a></dt>
+                        <dd>
+                            <p><a href="#">Responsive Bootstrap 3 Template placerat idelo alac eratamet.</a></p> 
+                        </dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt><a href="#"><img src="assets/img/sliders/elastislide/10.jpg" alt="" /></a></dt>
+                        <dd>
+                            <p><a href="#">100+ Amazing Features Layer Slider, Layer Slider, Icons, 60+ Pages etc.</a></p> 
+                        </dd>
+                    </dl>
+                    <dl class="dl-horizontal">
+                        <dt><a href="#"><img src="assets/img/sliders/elastislide/11.jpg" alt="" /></a></dt>
+                        <dd>
+                            <p><a href="#">Developer Friendly Code imperdiet condime ntumi mperdiet condim.</a></p> 
+                        </dd>
+                    </dl>
+                </div><!--/posts-->
+                <!-- End Post sebelumnya -->
+
+            </div>
+            <!-- End Right Sidebar -->
+        </div><!--/row-->        
+    </div><!--/container-->		
+    <!--=== End Content Part ===-->
