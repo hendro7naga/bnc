e503077e493a653effe7ff0b8db04fa420027084
xxcrashblackxx <crashblack160@gmail.com>
2015-12-16 02:44:51 +0700

    Create isi.php

---
+++ include/isi.php
@@ -0,0 +1,38 @@
+<?php
+require_once("./convikx/apps.php");
+?>
+<!--=== Breadcrumbs ===-->
+    <div class="breadcrumbs">
+        <div class="container">
+            <h1 class="pull-left">ERROR!</h1>
+            <ul class="pull-right breadcrumb">
+                <li><a href="index.html">Beranda</a></li>
+                <li><a href="">-</a></li>
+                <li class="active">ERROR!</li>
+            </ul>
+        </div><!--/container-->
+    </div><!--/breadcrumbs-->
+    <!--=== End Breadcrumbs ===-->
+
+    <!--=== Content ===-->
+    <div class="container">
+    <!-- Recent Works -->
+        <div class="row margin-bottom-20">
+            <div class="col-md-3 col-sm-6">
+                <div class="thumbnails thumbnail-style thumbnail-kenburn">
+                	<div class="thumbnail-img">
+                        <div class="overflow-hidden">
+                            <img class="img-responsive" src="img/gambar_default.jpg" alt="">
+                        </div>
+                        <a class="btn-more hover-effect" href="#">selengkapnya +</a>					
+                    </div>
+                    <div class="caption">
+                        <h3><a class="hover-effect" href="#">Error!</a></h3>
+                        <p>Maaf, ada kesalahan!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    	<!-- End Recent Works -->
+    	</div> <!-- akhir konten -->
+    <!--=== End Content ===-->
